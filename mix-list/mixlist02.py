1d614a3768a24e2e6a8a694e3439f4fc61cb378a
Androkles70 <alexandra69175@gmail.com>
2023-04-11 17:23:29 +0000

    manipulating lists

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,10 @@
+#! bin/bash/env python3
+
+wordbank= ["indentation", "spaces"]
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+wordbank.append(4)
+num= input ("6")
+
+
+#incomplete challenge 20
